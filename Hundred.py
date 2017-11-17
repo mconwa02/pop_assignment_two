5feaa8aad9155cbf2968df2e2772a26cb5f3d479
unknown <mconwa02@dcs.bbk.ac.uk>
2017-11-17 12:34:25 +0000

    Adding function names and defined roll function

--- Hundred.py
+++ Hundred.py
@@ -1,2 +1,37 @@
-'' Michelle Conway MCONWA02
-   Coded Solution to Hundred Game'''
+''' Michelle Conway MCONWA02
+   Coded Solutions to Hundred Game'''
+
+import random
+
+def main():
+    """ """
+def instruction():
+    """Rules of Game
+Players take turns to roll a die as many times or until 1 is rolled.
+Each number rolled, except a 1, is added to players score.
+If 1 is rolled score for this turn is zero and turn ends.
+At the end of each turn, the score for that turn is
+added to the player's total score.
+The first player to reach or exceed 100 wins.
+"""
+
+def human_move(computer_score, human_score):
+
+
+def computer_move(computer_score, human_score):
+    
+def is_game_over(computer_score, human_score):
+
+
+def roll():
+    die_value = random.randint(1,7)
+    return die_value
+
+print(roll())          
+
+def ask_yes_or_no(promt):
+    
+def show_results(computer_score, human_score):
+
+
+    
