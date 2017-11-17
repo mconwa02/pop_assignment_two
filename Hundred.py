9fa992a792af720c0b1e0362dd4d391e9340d957
unknown <mconwa02@dcs.bbk.ac.uk>
2017-11-17 16:20:12 +0000

    Defined ask_yes_or_no function

--- Hundred.py
+++ Hundred.py
@@ -20,6 +20,14 @@ The first player to reach or exceed 100 wins.
     computer move()
     
 def human_move(computer_score, human_score):
+""" Tells the user current score and computer's score
+and how far behind (or ahead) they are.
+Then repeatedly asks whether the user wants to roll again until,
+The user decides not to roll again or rolls a 1.
+The function returns the result (either 0 or the total of the rolls) """
+    ask_yes_or_no("Do you want to roll? ", {"y", "Y"})
+    if 
+
 
 
 def computer_move(computer_score, human_score):
@@ -39,7 +47,7 @@ The function returns the result (either 0 or the total of the rolls) """
         else:
             turn_sum = 0
             break
-        computer_score = turn_sum
+    computer_score = turn_sum
     if turn_sum ==0:
         result = 0
     else:
@@ -55,10 +63,32 @@ def roll():
 
 print(roll())          
 
-def ask_yes_or_no(promt, values):
-    return input(promt) in values
-    
-    
+def ask_yes_or_no(prompt):
+""" Prints the prompt as a question, "Roll again?"
+If responds starts with 'y' or 'Y' the function returns True.
+If responds starts with 'n' or 'N' the function returns False.
+Any other response will cause the question to be repeated"""
+    print(prompt)
+    s = input()
+    if s[0]=="y" or s[0] == "Y":
+        return True
+    elif s[0]=="n" or s[0] == "N":
+        return False
+    else:
+        print(prompt)
+        s = input()
+        while s[0]!="y" or s[0] != "Y" or s[0]!= "n" or s[0] != "N":
+            print(prompt)
+            s = input()
+            if s[0] == "y" or s[0] == "Y":
+                return  True
+                break
+            elif s[0] == "n" or s[0] == "N":
+                return  False
+                break
+
+ask_yes_or_no("Roll again?")
+
 def show_results(computer_score, human_score):
      """Declares weather the human won or lost"""
      if computor_score >100 and human_score < 100:
@@ -75,3 +105,6 @@ def test_answer():
     assert main(3) == 5
 
     
+
+
+
